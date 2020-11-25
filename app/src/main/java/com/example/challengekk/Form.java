212d315c487eb3657a6b4e093e6daeb832ec2102
sergey <sergey.ozernyy@mail.ru>
2020-11-25 22:35:38 +0300

    Кнопка Clear удаляет внесенные в фому значения. Кнопка Send создает объект Form, содержащий текущие значения полей формы и выводит на экран тост с ФИО (данные для тоста берутся из созданного объекта)

---
+++ app/src/main/java/com/example/challengekk/Form.java
@@ -0,0 +1,51 @@
+package com.example.challengekk;
+
+import android.widget.EditText;
+
+public class Form {
+    private String name;
+    private String division;
+    private String event;
+    private String theme;
+    private String content;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- app/src/main/java/com/example/challengekk/MainActivity.java
+++ app/src/main/java/com/example/challengekk/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.challengekk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final EditText nameEditText = (EditText) findViewById(R.id.name_editText);
+        final EditText divisionEditText = (EditText) findViewById(R.id.division_editText);
+        final EditText themeEditText = (EditText) findViewById(R.id.theme_editText);
+        final EditText contentEditText = (EditText) findViewById(R.id.content_editText);
+        final Spinner eventSpinner = (Spinner) findViewById(R.id.events_spinner);
+
+        Button clearButton = (Button) findViewById(R.id.clear_button);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nameEditText.setText("");
+                divisionEditText.setText("");
+                themeEditText.setText("");
+                contentEditText.setText("");
+                eventSpinner.setSelection(0);
+            }
+        });
+
+        Button sendButton = (Button) findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Form currentForm = new Form();
+                currentForm.setName(nameEditText.getText().toString());
+                currentForm.setDivision(divisionEditText.getText().toString());
+                currentForm.setTheme(themeEditText.getText().toString());
+                currentForm.setContent(contentEditText.getText().toString());
+                currentForm.setEvent(eventSpinner.getSelectedItem().toString());
+
+                Toast.makeText(getApplicationContext(),
+                        currentForm.getName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
