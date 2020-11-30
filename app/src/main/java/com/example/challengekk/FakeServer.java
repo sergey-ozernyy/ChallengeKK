00d1fcff1537426f88053296b3878eeae488c5d7
sergey <sergey.ozernyy@mail.ru>
2020-11-30 16:22:57 +0300

    Начал переделывать IntentService в Service, но толком еще ничего не успел

--- app/src/main/java/com/example/challengekk/FakeServer.java
+++ app/src/main/java/com/example/challengekk/FakeServer.java
@@ -1,71 +1,46 @@
 package com.example.challengekk;
 
 import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
 import android.content.Context;
+import android.os.Binder;
 import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * An {@link IntentService} subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * <p>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
- */
-public class FakeServer extends IntentService {
-    public String message;
+
+public class FakeServer extends Service {
+    private Looper mLooper;
+    private Handler mHandler;
+
+    private final IBinder binder = new FakeServerBinder();
+
+    public class FakeServerBinder extends Binder {
+        FakeServer getFakeServer() {
+            return FakeServer.this;
+        }
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public static final String ACTION_UPDATE_SEARCH = "updateSearchResult";
+    public static final String ACTION_UPDATE_SEND = "messageSend";
+    String message;
     Map<String, Form> allForms = new HashMap<>();
 
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FOO = "com.example.challengekk.action.FOO";
-    private static final String ACTION_BAZ = "com.example.challengekk.action.BAZ";
-
-    // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "com.example.challengekk.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "com.example.challengekk.extra.PARAM2";
-
-    public FakeServer() {
-        super("FakeServer");
-    }
-
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, FakeServer.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, FakeServer.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    private Handler mHandler;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -73,42 +48,39 @@ public class FakeServer extends IntentService {
         return super.onStartCommand(intent, flags, startId);
     }
 
-    @Override
+    //@Override
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
 
             Form form = (Form) intent.getSerializableExtra("sendForm");
-            if(form != null){
+            if (form != null) {
                 String themeForm = form.
                         getTheme();
-                if(allForms.containsKey(themeForm)){
+                if (allForms.containsKey(themeForm)) {
                     //Отрпавить в активность сообщение о том, то событие с такой темой уже существует
-                    message ="Событие с такой темой уже существует";
+                    message = "Событие с такой темой уже существует";
                 } else {
                     allForms.put(themeForm, form);
                     //Отправить сообщение о том, что событие добавлено в список
-                    message ="Событие добавлено в список";
+                    message = "Событие добавлено в список";
                 }
-
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(), message,
-                                Toast.LENGTH_LONG).show();
-                    }
-                });
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(ACTION_UPDATE_SEND);
+                sendIntent.addCategory(Intent.CATEGORY_DEFAULT);
+                sendIntent.putExtra("sendEvent", message);
+                sendBroadcast(sendIntent);
             }
 
             String searchTheme = intent.getStringExtra("request");
-            if (searchTheme != null){
+            if (searchTheme != null) {
                 ArrayList<String> resultSearch = new ArrayList<String>();
-                for (String key : allForms.keySet()){
-                    if (searchTheme.contains(key)){
+                for (String key : allForms.keySet()) {
+                    if (searchTheme.contains(key)) {
                         resultSearch.add(key);
                     }
                 }
                 Intent updateSearchResult = new Intent();
-                updateSearchResult.setAction("updateSearchResult");
+                updateSearchResult.setAction(ACTION_UPDATE_SEARCH);
                 updateSearchResult.addCategory(Intent.CATEGORY_DEFAULT);
                 updateSearchResult.putStringArrayListExtra("resultSearch", resultSearch);
                 sendBroadcast(updateSearchResult);
@@ -116,21 +88,5 @@ public class FakeServer extends IntentService {
         }
     }
 
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
 
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
 }
--- app/src/main/java/com/example/challengekk/MainActivity.java
+++ app/src/main/java/com/example/challengekk/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.challengekk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,16 +18,70 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public ArrayList<String> variablesAutocomplete;
+    private ArrayAdapter<String> autocompleteAdapter;
+    BroadcastReceiver mUpdateSearchBroadcastReceiver;
+    BroadcastReceiver mSendEventBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AutoCompleteTextView searchAutoCompleteText = (AutoCompleteTextView) findViewById(R.id.search_autoCompleteTextView);
-        
+        final AutoCompleteTextView searchAutoCompleteText = (AutoCompleteTextView) findViewById(R.id.search_autoCompleteTextView);
+        searchAutoCompleteText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (searchAutoCompleteText.getText().toString().length() >= 3){
+                    Intent searchRequest = new Intent(MainActivity.this, FakeServer.class);
+                    searchRequest.putExtra("request", searchAutoCompleteText.getText().toString());
+                    startService(searchRequest);
+                }
+            }
+        });
+
+        //Приемник для варинатов поиска
+        class UpdateSearchBroadcastReceiver extends BroadcastReceiver {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                variablesAutocomplete = intent.getStringArrayListExtra("resultSearch");
+            }
+        }
+
+        mUpdateSearchBroadcastReceiver = new UpdateSearchBroadcastReceiver();
+        IntentFilter updateIntentFilter = new IntentFilter(FakeServer.ACTION_UPDATE_SEARCH);
+        updateIntentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+        registerReceiver(mUpdateSearchBroadcastReceiver, updateIntentFilter);
+
+        //Адаптер для связывания поля автодополнения и вариантов для него
+        autocompleteAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, variablesAutocomplete);
+        searchAutoCompleteText.setAdapter(autocompleteAdapter);
+
+        //Приемник для события записи формы
+        class SendEventBroadcastReceiver extends BroadcastReceiver {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Toast.makeText(getApplicationContext(), intent.getStringExtra("sendEvent"), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        mSendEventBroadcastReceiver = new SendEventBroadcastReceiver();
+        IntentFilter sendIntentFilter = new IntentFilter(FakeServer.ACTION_UPDATE_SEND);
+        sendIntentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+        registerReceiver(mSendEventBroadcastReceiver, sendIntentFilter);
 
 
         final EditText nameEditText = (EditText) findViewById(R.id.name_editText);
@@ -63,5 +123,12 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, FakeServer.class);
         intent.putExtra("sendForm", (Serializable) currentForm);
         startService(intent);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(mUpdateSearchBroadcastReceiver);
+        unregisterReceiver(mSendEventBroadcastReceiver);
     }
 }
